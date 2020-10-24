96a5ca640f6df4347c71834f26739064007ca20b
Samuel Holmes <samuelholmes30@gmail.com>
2020-10-24 11:07:53 -0500

    changing to TSLA

--- src/stockApis/polygon.io.ts
+++ src/stockApis/polygon.io.ts
@@ -2,7 +2,7 @@ const axios = require('axios');
 export const getForexPrevClose = () => {
   return axios
     .get(
-      'https://api.polygon.io/v2/aggs/ticker/AAPL/prev?apiKey=0pZSRnSVRBxxsVLEBK_NCKmqUmnYcphI'
+      'https://api.polygon.io/v2/aggs/ticker/TSLA/prev?apiKey=0pZSRnSVRBxxsVLEBK_NCKmqUmnYcphI'
     )
     .then((res: any) => {
       console.log(res);
